e4b3010eabad460e9bafb578debc02ca479fa7c0
AdL1398 <adisorn.lert@gmail.com>
2018-04-12 13:27:22 +0100

    add PoC1 script

---
+++ source/demo_PoC1_SM.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+### stop NFD from the previous run 
+nfd-stop
+### stop DTN from the previous run 
+ps -ef | grep dtnd | grep -v grep | awk '{print $2}' | xargs kill
+### stop python script from the previous run 
+ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill
+### restart dtn by ponting to configuration file 
+dtnd -v -i wlan0 -c /home/pirate/umobile/ibr-dtn/demo-icn17-ibr.conf &
+sleep 10s 
+### restart NFD  
+nfd-start
+### create DTN face 
+#nfdc create dtn://umobile-pi-1/nfd
+
+## For testing 
+nfdc create tcp://192.168.0.248
+nfdc register  /picasso/service_deployment_push/SEG_1 tcp://192.168.0.248
---
+++ source/demo_PoC1_seg.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+### stop NFD from the previous run 
+nfd-stop
+### stop DTN from the previous run 
+ps -ef | grep dtnd | grep -v grep | awk '{print $2}' | xargs kill
+### stop python script from the previous run 
+ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill
+### stop all running docker containers 
+docker rm -f $(docker ps -a -q)
+### remove the docker image file (kebapp)
+rm /home/pirate/PiGeon/source/modules/ServiceExecution/SEG_repository/kebapp.tar.gz
+### restart dtn by ponting to configuration file 
+dtnd -v -i wlan0 -c /home/pirate/umobile/ibr-dtn/demo-icn17-ibr.conf &
+sleep 10s 
+### restart NFD  
+nfd-start
+### create DTN face
+#nfdc create dtn://umobile-pi-2/nfd
+### add NFD routing info to FIB by using DTN face
+#nfdc register /picasso/service_deployment_pull dtn://umobile-pi-2/nfd
+
+## For testing ### 
+nfdc create tcp://192.168.0.150
+nfdc register /picasso/service_deployment_pull tcp://192.168.0.150
